f3ab3d4ef5f7a7e508c3674de9a6aa8195d4bdfc
chalbrik <paw.swa95@gmail.com>
2024-02-08 18:08:13 +0100

    Update changes

--- index.js
+++ index.js
@@ -11,4 +11,17 @@ function checkViewportWidth() {
   window.addEventListener('load', checkViewportWidth);
   // Sprawdź przy zmianie rozmiaru okna
   window.addEventListener('resize', checkViewportWidth);
+
+
+  //kliknij przycisk hamburgera na stronie
+  document.getElementById("nav-bar-icon").addEventListener("click", () => {
+    document.getElementById("mobile-nav-bar-open").classList.toggle("show");
+    document.getElementById("mobile-nav-bar-overlay").classList.toggle("active");
+  });
+
+//kliknij na przycisk x na stronie
+  document.getElementById("close-nav-bar-icon").addEventListener("click", () => {
+    document.getElementById("mobile-nav-bar-open").classList.toggle("show");
+    document.getElementById("mobile-nav-bar-overlay").classList.toggle("active");
+  });
   
